b02095883599498829e1f211e2f91816446bf55b
sh3y <samanta.shreyas@icloud.com>
2025-02-07 12:05:21 +0000

    feat: media rendering

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -54,6 +54,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 TEMPLATES = [
     {
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -123,3 +125,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'

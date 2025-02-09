8e94ae7abb0f1b0698bcab13cea346ad9d39b5c0
sh3y <samanta.shreyas@icloud.com>
2025-02-09 16:36:46 +0000

    feat: urls

--- rango/models.py
+++ rango/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     
     def save(self, *args, **kwargs):
         # Testing chapter -- added test to ensure that views is non-negative.
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
+
 
 def index(request):
     
@@ -11,5 +13,19 @@ def index(request):
 
 
     return render(request, 'rango/index.html', context=context_dict)
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+        
+    return render(request, 'rango/category.html', context=context_dict)
 
 

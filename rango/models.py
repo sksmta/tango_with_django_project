02a8f1863d18531624d1208376aa38c92606fd4c
sh3y <samanta.shreyas@icloud.com>
2025-02-09 16:24:50 +0000

    feat: populating db & changing views

--- rango/models.py
+++ rango/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    NAME_MAX_LENGTH = 128
+    
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    
+    def save(self, *args, **kwargs):
+        # Testing chapter -- added test to ensure that views is non-negative.
+        if self.views < 0:
+            self.views = 0
+        
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
     
     class Meta:
         verbose_name_plural = 'Categories'
